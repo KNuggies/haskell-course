af11b68c033df1939862ab96189646104bab3f06
JBarCode37 <jason.barhorst@jbarcode.com>
2021-06-02 22:37:31 +0000

    Go to Prob 0 to see links to problems w/o solution

---
+++ CodeWars/8-kyu/prob0.hs
@@ -0,0 +1,9 @@
+-- Start here to find the problems without the temptation of glimpsing at the solutions
+-- Problem 1: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+-- Problem 2: https://www.codewars.com/kata/57a429e253ba3381850000fb
+-- Problem 3: https://www.codewars.com/kata/5f70c883e10f9e0001c89673
+-- Problem 4: https://www.codewars.com/kata/55a70521798b14d4750000a4
+-- Problem 5: https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+-- Problem 6: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+-- Problem 7: https://www.codewars.com/kata/5966e33c4e686b508700002d
+ 
--- CodeWars/8-kyu/prob1.hs
+++ CodeWars/8-kyu/prob1.hs
@@ -1,4 +1,4 @@
--- https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/haskell
+-- https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 
 evenOrOdd :: Integral a => a -> [Char]
 evenOrOdd n = if even n then "Even" else "Odd"
